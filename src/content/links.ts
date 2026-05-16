4f05e90fa50c279c721cfb002610b14e0e1eb0dd
Ink6x <jsnowman.8118@gmail.com>
2026-05-16 13:37:33 +0900

    feat(contact/about): add X link (https://x.com/xInk6x)

--- src/content/links.ts
+++ src/content/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   email: "humanxxplus@gmail.com" as string | undefined,
-  x: undefined as string | undefined,
+  x: "https://x.com/xInk6x" as string | undefined,
   github: "https://github.com/Ink6x" as string | undefined,
   stackshare: undefined as string | undefined,
 };
